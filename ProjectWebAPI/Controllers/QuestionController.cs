b94a254ab91612ee41bbd0aff210f5de731636a2
SamMillerSA <37656779+SamMillerSA@users.noreply.github.com>
2018-10-11 17:02:41 +1030

    Update ProjectWebAPI/Controllers/QuestionController.cs

---
+++ ProjectWebAPI/Controllers/QuestionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWebAPI.Controllers
+{
+   // [Produces("application/json")]
+    [Route("api/Question")]
+    public class QuestionController : Controller
+    {
+        // GET: api/Question
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "question1", "question2" };
+        }
+
+        // GET: api/Question/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Question
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Question/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

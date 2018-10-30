729d8bdf8361e3ae39f04099b85632c46e9b77fb
gerritrmit <43431870+gerritrmit@users.noreply.github.com>
2018-10-30 15:39:54 +1100

    Add files via upload

---
+++ majorProjectFrontEnd/MajorProjectFrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MajorProjectFrontEnd.Models;
+using MajorProjectFrontEnd.Services;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.AspNetCore.Http;
+
+namespace MajorProjectFrontEnd.Controllers
+{
+	public class HomeController : Controller
+	{
+		DatabaseService databaseService = new DatabaseService();
+
+		HttpClient client = new HttpClient();
+
+		[HttpGet("{id}")]
+		public IActionResult Index(int id)
+		{
+			List<QuestionDataModel> questionData = new List<QuestionDataModel>();
+
+			questionData = databaseService.GetQuestionData(id);
+
+			return View(questionData);
+		}
+
+		public async Task<List<QuestionDataModel>> getQuestionDataAsync()
+		{
+			// https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+
+			// What string do I put here?
+			client.BaseAddress = new Uri("http://localhost:64195/");
+
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(
+			    new MediaTypeWithQualityHeaderValue("application/json"));
+
+			List<QuestionDataModel> list = null;
+
+
+			HttpResponseMessage response = await client.GetAsync("api/surveyQuestions");
+			if (response.IsSuccessStatusCode)
+			{
+				list = await response.Content.ReadAsAsync<List<QuestionDataModel>>();
+			}
+			return list;
+
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+
+		public ViewResult SurveyResponse()
+		{
+			return View("SurveyResponse", GetResponseModelList(Request.Form));
+		}
+
+
+
+		private List<ResponseModel> GetResponseModelList(IFormCollection col)
+		{
+			int numberOfQuestions = Int32.Parse(col["numberOfQuestions"]);
+			int surveyID = Int32.Parse(col["surveyID"]);
+			List<ResponseModel> list = new List<ResponseModel>();
+
+			for (int i = 1; i <= numberOfQuestions; ++i)
+			{
+				string questionOptions = col["question_options_" + i.ToString()];
+				string questionTitle = col["question_title_" + i.ToString()];
+				string questionType = col["question_type_" + i.ToString()];
+				string response = col[i.ToString()];
+
+				var responseModel = new ResponseModel { surveyID = surveyID, options = questionOptions, questionType = questionType, 
+									question = questionTitle, questionNumber = i, response = response };
+
+				list.Add(responseModel);
+			}
+
+			return list;
+		}
+	}
+}
---
+++ majorProjectFrontEnd/MajorProjectFrontEnd/wwwroot/js/site.js
@@ -0,0 +1,74 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+
+    $('select.rank').on('change', function (e) {
+        var selections = [];
+
+        $('select').filter("[name=" + "'" + e.target.getAttribute("name") + "'" + "]").each(function () {
+            selections.push($(this).val());
+        });
+
+        for (i = 0; i < selections.length; ++i) {
+            console.log(selections[i]);
+        }
+
+        console.log("\n");
+
+
+        if (selections.length == $.unique(selections).length) {
+            $("p.errorMessage").filter("." + $(this).attr('name')).html("");
+
+        }
+        else {
+            $("p.errorMessage").filter("." + $(this).attr('name')).html("Duplicates are not allowed.");
+            
+        }
+
+    });
+
+
+
+    $('#menu-bar').on('click', function (e) {
+        var nav = document.getElementById("nav");
+
+        if (nav.className == "nav-hide") {
+
+            nav.className = "nav-show";
+        }
+        else {
+
+            nav.className = "nav-hide";
+        }
+
+    });
+
+
+
+    $("#target").submit(function(e) {
+        var invalid = false;
+        $("p.errorMessage").each(function () {
+
+
+            if ($(this).html() != "") {
+                $("#mainErrorMessage").html("There are errors. Please review your responses.");
+                invalid = true;
+                return false;
+            }
+            else
+            {
+                $("#mainErrorMessage").html("");
+            }
+        }); 
+
+        if (invalid == true) {
+            return false;
+        }
+    });
+
+
+});

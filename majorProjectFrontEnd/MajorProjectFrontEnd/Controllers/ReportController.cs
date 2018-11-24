15aaf56428153b6a87297c0a06076b70d4f9aeac
gerritrmit <s3537790@student.rmit.edu.au>
2018-11-24 12:44:23 +1100

    Update ReportController.cs

--- majorProjectFrontEnd/MajorProjectFrontEnd/Controllers/ReportController.cs
+++ majorProjectFrontEnd/MajorProjectFrontEnd/Controllers/ReportController.cs
@@ -2,92 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MajorProjectFrontEnd.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MajorProjectFrontEnd.Controllers
 {
-    public class ReportController : Controller
-    {
-        // GET: Report
-        public ActionResult Index()
-        {
-            return View();
-        }
+	[Route("[controller]")]
+	public class ReportController : Controller
+	{
 
-        // GET: Report/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		String baseAddress = "https://projectwebapis.azurewebsites.net/";
+		String requestUri = null;
 
-        // GET: Report/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		APIService api = new APIService();
 
-        // POST: Report/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+		// GET: Report
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		[HttpGet("{fileName}")]
+		public ActionResult Index(string fileName)
+		{
+			return View();
+		}
 
-        // GET: Report/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		// GET: Report/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
 
-        // POST: Report/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+		// GET: Report/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		// POST: Report/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
 
-        // GET: Report/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-        // POST: Report/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+		// GET: Report/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		// POST: Report/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
+
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: Report/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: Report/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
+
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
